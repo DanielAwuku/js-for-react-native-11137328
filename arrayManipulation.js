0da6de159407ef9e3d3c42e2e47082d2776d2a25
dnawuku@st.ug.edu.gh <danielgodfred12@gmail.com>
2024-05-18 14:30:24 +0000

    task 1

---
+++ arrayManipulation.js
@@ -0,0 +1,13 @@
+// arrayManipulation.js
+
+function processArray(numbers) {
+    return numbers.map(number => {
+        if (number % 2 === 0) {
+            return number * number; 
+        } else {
+            return number * 3; 
+        }
+    });
+}
+
+module.exports = { processArray };
