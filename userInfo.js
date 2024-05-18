233b4160a7a827021fe202b11aa46f62d81695fa
dnawuku@st.ug.edu.gh <danielgodfred12@gmail.com>
2024-05-18 15:48:34 +0000

    task 3

---
+++ userInfo.js
@@ -0,0 +1,11 @@
+// createUserProfiles
+
+function createUserProfiles(names, modifiedNames) {
+    return names.map((name, index) => ({
+        id: index + 1,
+        originalName: name,
+        modifiedName: modifiedNames[index]
+    }));
+}
+
+module.exports = { createUserProfiles };
