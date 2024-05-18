9802ba6b8a9de4b5722020568ff0a9754d0a5b85
dnawuku@st.ug.edu.gh <danielgodfred12@gmail.com>
2024-05-18 14:41:02 +0000

    task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,4 +1,4 @@
-// arrayManipulation.js
+// processArray
 
 function processArray(numbers) {
     return numbers.map(number => {
@@ -11,3 +11,17 @@ function processArray(numbers) {
 }
 
 module.exports = { processArray };
+
+
+// formatArrayStrings
+function formatArrayStrings(strings, numbers) {
+    return strings.map((str, index) => {
+        if (numbers[index] % 2 === 0) {
+            return str.toUpperCase(); 
+        } else {
+            return str.toLowerCase(); 
+        }
+    });
+}
+
+module.exports = { processArray, formatArrayStrings };
